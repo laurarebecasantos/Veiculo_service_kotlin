483d79833a1ab8e59363c7b000ce24700f7a59be
Laura Santos <laurarebecasantos@gmail.com>
2022-02-16 03:03:19 -0300

    Package service

---
+++ veiculo-service/src/main/kotlin/br/com/curso/service/VeiculoService.kt
@@ -0,0 +1,18 @@
+package br.com.curso.service
+
+import br.com.curso.model.Veiculo
+import br.com.curso.repository.VeiculoRepository
+import jakarta.inject.Singleton
+
+@Singleton
+class VeiculoService(
+    private val veiculoRepository: VeiculoRepository
+) {
+    fun create(veiculo: Veiculo): Veiculo {
+        return veiculoRepository.save(veiculo)
+    }
+
+    fun findByIb(id: Long): Veiculo {
+        return veiculoRepository.findById(id).get()
+    }
+}
